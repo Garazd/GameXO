f057682ba2b5483db582cb33471576812d107bfc
Vitaliy <GarazdVZ@gmail.com>
2015-07-08 21:40:46 +0300

    Add PlayerTest

---
+++ test/model/PlayerTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetName() {
+        final String inputValue = "Vitaliy";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testGetFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
