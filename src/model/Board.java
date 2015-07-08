efc786ab21bc6452361508e5c5fa7fe80dc2a15c
Vitaliy <GarazdVZ@gmail.com>
2015-07-08 22:27:47 +0300

    Update Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -5,6 +5,8 @@ import java.awt.*;
 public class Board {
 
     private static final int SIZE_FIELD = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = SIZE_FIELD;
 
     private final Figure[][] figures = new Figure[SIZE_FIELD][SIZE_FIELD];
 
@@ -18,5 +20,13 @@ public class Board {
 
     public void setFigure(final Point point, final Figure figure) {
         figures [point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 }
