9ed6639c59b90dfaeab7b7fdce8a69edbd40c9b4
Vitaliy <GarazdVZ@gmail.com>
2015-07-08 22:19:33 +0300

    Update Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,25 +1,22 @@
 package model;
 
-import helpers.CoordinateHelper;
+import java.awt.*;
 
 public class Board {
 
     private static final int SIZE_FIELD = 3;
 
-    private Figure[][] figures = new Figure[SIZE_FIELD][SIZE_FIELD];
+    private final Figure[][] figures = new Figure[SIZE_FIELD][SIZE_FIELD];
 
-    public Figure getFigure(final int x, final int y) {
-        if (!CoordinateHelper.checkCoordinate(x) || !CoordinateHelper.checkCoordinate(y)) return null;
-            return figures [x][y];
+    public int getSize(){
+        return SIZE_FIELD;
     }
 
-    public boolean setFigure(final int x, final int y, final Figure figure) {
-        if (!CoordinateHelper.checkCoordinate(x) || !CoordinateHelper.checkCoordinate(y))
-        return false;
+    public Figure getFigure(final Point point) {
+            return figures [point.x][point.y];
+    }
 
-        if (figures[x][y] != null) return false;
-
-        figures[x][y] = figure;
-        return true;
+    public void setFigure(final Point point, final Figure figure) {
+        figures [point.x][point.y] = figure;
     }
 }
