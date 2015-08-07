3331739ea03bb4bf1230229c79dbcf892fdc63a6
Vitaliy <GarazdVZ@gmail.com>
2015-08-07 23:54:13 +0300

    Add CurrentMoveController

---
+++ src/controller/CurrentMoveController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import model.Board;
+import model.Figure;
+import model.exeptions.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Board board) {
+        int countFigure = 0;
+        for (int x = 0; x < board.getSize(); x++) {
+            countFigure += countFiguresInTheRow(board, x);
+        }
+
+        if (countFigure == board.getSize() * board.getSize())
+            return null;
+
+        if (countFigure % 2 == 0)
+            return Figure.X;
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Board board, final int row) {
+        int countFigure = 0;
+        for (int x = 0; x < board.getSize(); x++) {
+            try {
+                if (board.getFigure(new Point(x, row)) != null)
+                    countFigure++;
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+        return countFigure;
+    }
+}
