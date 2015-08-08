7e733c4c11f9b4ae1bf077058f1e7a6a6e64f7b0
Vitaliy <GarazdVZ@gmail.com>
2015-08-08 10:25:27 +0300

    Update CurrentMoveControllerTest

--- test/controller/CurrentMoveControllerTest.java
+++ test/controller/CurrentMoveControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.awt.*;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 
 public class CurrentMoveControllerTest {
 
@@ -33,5 +34,21 @@ public class CurrentMoveControllerTest {
             board.setFigure(new Point(i, 2), Figure.X);
             assertEquals(Figure.X, currentMoveController.currentMove(board));
         }
+    }
+
+    @Test
+    public void testCurrentMoveWhenNoNextMove() throws AlreadyOccupiedException, InvalidPointException {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Board board = new Board();
+        board.setFigure(new Point(0, 0), Figure.O);
+        board.setFigure(new Point(0, 1), Figure.X);
+        board.setFigure(new Point(0, 2), Figure.O);
+        board.setFigure(new Point(1, 0), Figure.O);
+        board.setFigure(new Point(1, 1), Figure.X);
+        board.setFigure(new Point(1, 2), Figure.O);
+        board.setFigure(new Point(2, 0), Figure.O);
+        board.setFigure(new Point(2, 1), Figure.X);
+        board.setFigure(new Point(2, 2), Figure.O);
+        assertNull(currentMoveController.currentMove(board));
     }
 }
