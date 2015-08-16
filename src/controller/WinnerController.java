e58b00c38574bd271c167bc33f1f6c8bb2f8051c
Vitaliy <GarazdVZ@gmail.com>
2015-08-16 10:30:28 +0300

    Update WinnerController

--- src/controller/WinnerController.java
+++ src/controller/WinnerController.java
@@ -2,28 +2,27 @@ package controller;
 
 import model.Board;
 import model.Figure;
+import model.Point;
 import model.exeptions.InvalidPointException;
-
-import java.awt.*;
 
 public class WinnerController {
 
     public static Figure getWinner(final Board board) {
         try {
             for(int i = 0; i < 3; i++)
-                if (check(board, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
+                if (check(board, new Point(i, 0), p -> new Point(p.getX(), p.getY() + 1)))
                     return board.getFigure(new Point(i, 0));
 
             for(int i = 0; i < 3; i++)
-                if (check(board, new Point(0, i), p -> new Point(p.x + 1, p.y)))
+                if (check(board, new Point(0, i), p -> new Point(p.getX() + 1, p.getY())))
                     return board.getFigure(new Point(0, i));
 
             for(int i = 0; i < 3; i++)
-                if (check(board, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                if (check(board, new Point(0, 0), p -> new Point(p.getX() + 1, p.getY() + 1)))
                     return board.getFigure(new Point(0, 0));
 
             for(int i = 0; i < 3; i++)
-                if (check(board, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
+                if (check(board, new Point(0, 2), p -> new Point(p.getX() + 1, p.getY() - 1)))
                     return board.getFigure(new Point(1, 1));
         } catch (final InvalidPointException e) {
             e.printStackTrace();
