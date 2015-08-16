c2b3a950fee728a2799cae87801491e4ac4d0524
Vitaliy <GarazdVZ@gmail.com>
2015-08-16 10:29:37 +0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,39 @@ import model.Board;
 import model.Figure;
 import model.Game;
 import model.Player;
+import model.exeptions.InvalidBoardSizeException;
 import view.ConsoleView;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(final String[] args) {
 
-        final Board board = new Board();
+        final int FIELD_SIZE = 3;
+
         final Player[] players = new Player[2];
         players[0] = new Player("Vitaliy", Figure.X);
         players[1] = new Player("Oleg", Figure.O);
 
-        final Game gameXO = new Game(players, board, "XO");
-
-        final ConsoleView consoleView = new ConsoleView();
-        consoleView.showBoard(gameXO);
-        while (consoleView.move(gameXO)) {
-            consoleView.showBoard(gameXO);
+        Board board = null;
+        try {
+            board = new Board(FIELD_SIZE);
+        } catch (InvalidBoardSizeException e) {
+            e.printStackTrace();
         }
+        final Game gameXO = new Game(players, board, "XO");
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+        while (consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
+        }
+    }
+
+    static String playerNameInput(final int count) {
+        Scanner sc = new Scanner(System.in);
+        System.out.format("Enter Player %s  name: ", count);
+        String name = sc.nextLine();
+        return name;
     }
 }
