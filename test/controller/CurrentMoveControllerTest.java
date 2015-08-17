2a54dfad0158a4a1748fabacf14fefa13ac46aaa
Vitaliy <GarazdVZ@gmail.com>
2015-08-17 21:13:48 +0300

    Update CurrentMoveControllerTest

--- test/controller/CurrentMoveControllerTest.java
+++ test/controller/CurrentMoveControllerTest.java
@@ -2,11 +2,8 @@ package controller;
 
 import model.Board;
 import model.Figure;
-import model.exeptions.AlreadyOccupiedException;
-import model.exeptions.InvalidPointException;
+import model.Point;
 import org.junit.Test;
-
-import java.awt.*;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
@@ -14,10 +11,10 @@ import static junit.framework.Assert.assertNull;
 public class CurrentMoveControllerTest {
 
     @Test
-    public void testCurrentMoveWhenNextMoveIsO() throws AlreadyOccupiedException, InvalidPointException {
+    public void testCurrentMoveWhenNextMoveIsO() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
         for (int i = 0; i < 3; i++) {
-            final Board board = new Board();
+            final Board board = new Board(3);
             board.setFigure(new Point(i, 0), Figure.X);
             board.setFigure(new Point(i, 1), Figure.O);
             board.setFigure(new Point(i, 2), Figure.X);
@@ -26,10 +23,10 @@ public class CurrentMoveControllerTest {
     }
 
     @Test
-    public void testCurrentMoveWhenNextMoveIsX() throws AlreadyOccupiedException, InvalidPointException {
+    public void testCurrentMoveWhenNextMoveIsX() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
         for (int i = 0; i < 3; i++) {
-            final Board board = new Board();
+            final Board board = new Board(3);
             board.setFigure(new Point(i, 1), Figure.O);
             board.setFigure(new Point(i, 2), Figure.X);
             assertEquals(Figure.X, currentMoveController.currentMove(board));
@@ -37,9 +34,9 @@ public class CurrentMoveControllerTest {
     }
 
     @Test
-    public void testCurrentMoveWhenNoNextMove() throws AlreadyOccupiedException, InvalidPointException {
+    public void testCurrentMoveWhenNoNextMove() throws Exception {
         final CurrentMoveController currentMoveController = new CurrentMoveController();
-        final Board board = new Board();
+        final Board board = new Board(3);
         board.setFigure(new Point(0, 0), Figure.O);
         board.setFigure(new Point(0, 1), Figure.X);
         board.setFigure(new Point(0, 2), Figure.O);
