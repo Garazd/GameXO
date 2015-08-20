a9f66f6725af5821af8cf3d02922dd8c05d27c53
Vitaliy <GarazdVZ@gmail.com>
2015-08-20 20:27:26 +0300

    Update Figure

--- src/model/Figure.java
+++ src/model/Figure.java
@@ -1,18 +1,5 @@
 package model;
 
 public enum Figure {
-
-    X("X"),O("O");
-
-    private final String figure;
-
-    Figure(final String figure) {
-        assert figure != null;
-
-        this.figure = figure;
-    }
-
-    public String getFigure() {
-        return figure;
-    }
+    X,O
 }
