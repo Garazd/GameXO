700e2ce73fa2724a7f2849ee4fa91e9c228c404a
Vitaliy <GarazdVZ@gmail.com>
2015-08-20 21:14:39 +0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,24 @@ import view.ConsoleView;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(final String[] args) {
-
         final int FIELD_SIZE = 3;
+        final String name1 = playerNameInput(1);
+        final String name2 = playerNameInput(2);
 
         final Player[] players = new Player[2];
-        players[0] = new Player("Vitaliy", Figure.X);
-        players[1] = new Player("Oleg", Figure.O);
-
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
         Board board = null;
         try {
             board = new Board(FIELD_SIZE);
-        } catch (InvalidBoardSizeException e) {
+        } catch (InvalidBoardSizeException e){
             e.printStackTrace();
         }
         final Game gameXO = new Game(players, board, "XO");
         final ConsoleView consoleView = new ConsoleView();
         consoleView.show(gameXO);
-        while (consoleView.move(gameXO)) {
+        while(consoleView.move(gameXO)) {
             consoleView.show(gameXO);
         }
     }
